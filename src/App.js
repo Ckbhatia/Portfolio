b5ccb8018def54b400b5c8ea5d9897567528d988
Ckbhatia <chetansain86@gmail.com>
2018-12-07 19:46:47 +0530

    fix: replaced component Navbar to Main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar.jsx';
+import Main from './components/Main.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Main />
       </div>
     );
   }
