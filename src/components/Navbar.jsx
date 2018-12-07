6eabfa237ba40b05801b262aadd274b4c206b96a
Ckbhatia <chetansain86@gmail.com>
2018-12-07 16:26:58 +0530

    feat: modified for heading

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React, { component } from 'react';
+import '../App.css';
 
 const Navbar=()=> {
     return (
-        <div>
-            <nav id="nav">
-                <ul>
+        <header>
+            <div>
+            <nav id="navbar">
+                <ul id="nav">
 				    <li><a href="#home">Home</a></li>
 					<li><a href="#projects">Projects</a></li>
 					<li><a href="#contact">Contact</a></li>
@@ -12,6 +14,8 @@ const Navbar=()=> {
 			    </ul>
             </nav>
         </div>
+            <h1>Chetan Kumar's Portfolio</h1>    
+        </header>
     )
 }
 export default Navbar;
