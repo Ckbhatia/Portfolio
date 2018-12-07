f369f30c26a1be277c19d64c91f304616e0675b0
Ckbhatia <chetansain86@gmail.com>
2018-12-07 19:48:40 +0530

    feat: created new component

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../App.css';
+import Navbar from './Navbar.jsx';
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <main id="main">
+                    
+                </main>
+            </div>
+        )
+    }
+}
