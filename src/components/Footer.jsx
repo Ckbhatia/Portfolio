d3e14535f78d44d13671f85e03b74e605dd38f18
Ckbhatia <chetansain86@gmail.com>
2018-12-08 20:34:34 +0530

    feat: add rel attribute to links with social links

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const Footer=()=> {
+    return (
+        <div role="link" class="social-links">
+        <a href="https://www.linkedin.com/in/chetan-kumar86" target="_blank" rel="noopener noreferrer" >Linkedin</a>
+        <a href="https://github.com/Ckbhatia" target="_blank" rel="noopener noreferrer" >Github</a>
+        <a href="https://twitter.com/chetanbhatias" target="_blank" rel="noopener noreferrer" >Twitter</a>
+        {/*<a href="#medium" target="_blank" rel="noopener noreferrer" >medium</a>*/}
+        </div>
+    )
+}
+export default Footer;
