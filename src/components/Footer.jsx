0510411fac1c6209bc4b15e1747d36d80ae09d28
Ckbhatia <chetansain86@gmail.com>
2018-12-10 17:33:57 +0530

    feat: added className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import '../App.css';
 
 const Footer=()=> {
     return (
-        <div role="link" class="social-links">
-        <a href="https://www.linkedin.com/in/chetan-kumar86" target="_blank" rel="noopener noreferrer" >Linkedin</a>
-        <a href="https://github.com/Ckbhatia" target="_blank" rel="noopener noreferrer" >Github</a>
-        <a href="https://twitter.com/chetanbhatias" target="_blank" rel="noopener noreferrer" >Twitter</a>
+        <div role="link" className="social-links">
+        <a className="link" href="https://www.linkedin.com/in/chetan-kumar86" target="_blank" rel="noopener noreferrer" >Linkedin</a>
+        <a className="link" href="https://github.com/Ckbhatia" target="_blank" rel="noopener noreferrer" >Github</a>
+        <a className="link" href="https://twitter.com/chetanbhatias" target="_blank" rel="noopener noreferrer" >Twitter</a>
         {/*<a href="#medium" target="_blank" rel="noopener noreferrer" >medium</a>*/}
         </div>
     )
