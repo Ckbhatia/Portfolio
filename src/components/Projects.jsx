a04f6613536ba13f3dc1ca5be1e0640dd89bad42
Ckbhatia <chetansain86@gmail.com>
2018-12-14 14:52:37 +0530

    feat: created FeaturedProjects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../App.css';
 
+function importAll(r) {
+    return r.keys().map(r);
+  }
+const images = importAll(require.context('./', false, /\.(png|jpe?g|svg)$/));
 
-const Projects =()=> {
-    const projectText = ['React Neighborhood map', 'React My reads', 'Restaurant Reviews App', 'Feed Reader Testing', 'Classic Arcade Game', 'Memory Game', 'Random Color picker', 'Portfolio', 'Pixel Art maker', 'Animal Trading Cards'];
+export const FeaturedProjects =()=> { 
+        
+    const featuredProjectName = ['React Neighborhood Map', 'Restaurant Reviews App', 'My Reads'];
+    
+    return (
+        featuredProjectName.map(name => 
+            <div className="project">
+                    <h3>{name}</h3>
+                    <img src={images[0]} alt={name}/>
+                    <a role="button" className="btn" href="https://" target="_blank" rel="noopener noreferrer">Github</a>
+                    </div>
+            )
+               
+)};
+
+
+export const Projects =()=> {
+    const projectName = ['React Neighborhood map', 'React My reads', 'Restaurant Reviews App', 'Feed Reader Testing', 'Classic Arcade Game', 'Memory Game', 'Random Color picker', 'Portfolio', 'Pixel Art maker', 'Animal Trading Cards'];
 
     return (
-            projectText.map((project, index) => (
+            projectName.map((project, index) => (
             <div key={index} className="project">
             <h3>{project}</h3>
             <img src={''} alt={project}/>
@@ -15,4 +35,3 @@ const Projects =()=> {
         ))
     )
 };
-export default Projects;
