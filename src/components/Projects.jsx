91ecb570b660362851a4c96403a6c3b7fb30a353
Ckbhatia <chetansain86@gmail.com>
2018-12-17 19:11:33 +0530

    feat: added a new button for LIVE view Project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import '../App.css';
 
-/*Testing */
+/* Importing images by webpack */
 function importAll(r) {
     return r.keys().map(r);
   }
   
-const images = importAll(require.context('./', false, /\.(png|jpe?g|svg)$/));
-console.log(images);
+const images = importAll(require.context('../img/projects', false, /\.(png|jpe?g|svg)$/));
+
+//Experiments
+
 
 export const FeaturedProjects =()=> { 
         
     const featuredProjectName = ['React Neighborhood Map', 'Restaurant Reviews App', 'My Reads App', 'Feed Reader Testing'];
     
+
     return (
         featuredProjectName.map((name, index) => 
             <div key={index} className="project">
+                    <div className="part1">
                     <h3>{name}</h3>
-                    <img src={images[index]} alt={name}/>
-                    <a role="button" className="btn" href="https://" target="_blank" rel="noopener noreferrer">Github</a>
+                    <img className="pro-img" src={images[index]} alt={name}/>
                     </div>
-            )
-               
+                    <div className="part2">
+                    <a role="button" className="btn" href="https://" target="_blank" rel="noopener noreferrer">Github</a>
+                    <a role="button" className="btn2" href="https://" target="_blank" rel="noopener noreferrer">Live</a>
+                    </div>
+                    </div>
+            )               
 )};
 
 
@@ -31,9 +38,14 @@ export const Projects =()=> {
     return (
             projectName.map((project, index) => (
             <div key={index} className="project">
+            <div className="part1">
             <h3>{project}</h3>
-            <img src={''} alt={project}/>
+            <img src={images[index]} alt={project}/>
+            </div>
+            <div className="part2">
             <a role="button" className="btn" href={`https://${project}`} target="_blank" rel="noopener noreferrer">Github</a>
+            <a role="button" className="btn2" href={`https://${project}`} target="_blank" rel="noopener noreferrer">Live</a>
+            </div>
             </div>
         ))
     )
