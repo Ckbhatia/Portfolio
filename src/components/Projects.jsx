c6242318fbb093d4b0a6c7c303d8df5f0b513682
Ckbhatia <chetansain86@gmail.com>
2018-12-19 20:58:14 +0530

    feat: added Codepen name for link for some projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,7 +14,7 @@ const images = importAll(require.context('../img/projects', false, /\.(png|jpe?g
 export const FeaturedProjects =()=> { 
         
     const featuredProjectsName = ['React Neighborhood Map', 'Restaurant Reviews App', 'My Reads App', 'Feed Reader Testing'];
-    const featuredProjectsUrl = ['Neighborhood-Map-React','mws-restaurant-stage-1','reactnd-myreads','frontend-nanodegree-feedreader'];
+    const featuredProjectsUrl = ['Neighborhood-Map-React', 'mws-restaurant-stage-1', 'reactnd-myreads', 'frontend-nanodegree-feedreader'];
 
     return (
         featuredProjectsName.map((name, index) => 
@@ -34,16 +34,18 @@ export const FeaturedProjects =()=> {
 
 export const Projects =()=> {
     const projectsName = ['Classic Arcade Game', 'Memory Game', 'Random Color picker', 'Portfolio', 'Pixel Art maker', 'Animal Trading Cards'];
-    const projectsUrl = ['frontend-nanodegree-arcade-game','fend-project-memory-game','React-random-color-picker','Ckbhatia.github.io','',''];
+    const projectsUrl = ['frontend-nanodegree-arcade-game', 'fend-project-memory-game', 'React-random-color-picker', 'Ckbhatia.github.io', 'project/editor/XWMgkQ', 'pen/zWbVwE'];
+
     return (
             projectsName.map((project, index) => (
             <div key={index} className="project">
             <div className="part1">
-            <h3>{project}</h3>
+            <h3>{project}</h3>  
             <img src={images[index]} alt={project}/>
             </div>
             <div className="part2">
-            <a role="button" className="btn" href={`https://github.com/Ckbhatia/${projectsUrl[index]}`} target="_blank" rel="noopener noreferrer">Github</a>
+            {/* Button for gihub and live view the projects */}
+            <a role="button" className="btn" href={index <= 3 ? `https://github.com/Ckbhatia/${projectsUrl[index]}` :  `https://codepen.io/ckbhatia/${projectsUrl[index]}`} target="_blank" rel="noopener noreferrer">{index <= 3 ? `Github`: 'Codepen'}</a>
             <a role="button" className="btn2" href={`https://${projectsUrl}`} target="_blank" rel="noopener noreferrer">Live</a>
             </div>
             </div>
