ccf4b365118d2394f76213394e2d6c76b694d9d8
Ckbhatia <chetansain86@gmail.com>
2018-12-19 18:36:03 +0530

    fix: added available gitub links of projects and minor changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,18 +13,18 @@ const images = importAll(require.context('../img/projects', false, /\.(png|jpe?g
 
 export const FeaturedProjects =()=> { 
         
-    const featuredProjectName = ['React Neighborhood Map', 'Restaurant Reviews App', 'My Reads App', 'Feed Reader Testing'];
-    
+    const featuredProjectsName = ['React Neighborhood Map', 'Restaurant Reviews App', 'My Reads App', 'Feed Reader Testing'];
+    const featuredProjectsUrl = ['Neighborhood-Map-React','mws-restaurant-stage-1','reactnd-myreads','frontend-nanodegree-feedreader'];
 
     return (
-        featuredProjectName.map((name, index) => 
+        featuredProjectsName.map((name, index) => 
             <div key={index} className="project">
                     <div className="part1">
                     <h3>{name}</h3>
                     <img className="pro-img" src={images[index]} alt={name}/>
                     </div>
                     <div className="part2">
-                    <a role="button" className="btn" href="https://" target="_blank" rel="noopener noreferrer">Github</a>
+                    <a role="button" className="btn" href={`https://github.com/Ckbhatia/${featuredProjectsUrl[index]}`} target="_blank" rel="noopener noreferrer">Github</a>
                     <a role="button" className="btn2" href="https://" target="_blank" rel="noopener noreferrer">Live</a>
                     </div>
                     </div>
@@ -33,18 +33,18 @@ export const FeaturedProjects =()=> {
 
 
 export const Projects =()=> {
-    const projectName = ['Classic Arcade Game', 'Memory Game', 'Random Color picker', 'Portfolio', 'Pixel Art maker', 'Animal Trading Cards'];
-
+    const projectsName = ['Classic Arcade Game', 'Memory Game', 'Random Color picker', 'Portfolio', 'Pixel Art maker', 'Animal Trading Cards'];
+    const projectsUrl = ['frontend-nanodegree-arcade-game','fend-project-memory-game','React-random-color-picker','Ckbhatia.github.io','',''];
     return (
-            projectName.map((project, index) => (
+            projectsName.map((project, index) => (
             <div key={index} className="project">
             <div className="part1">
             <h3>{project}</h3>
             <img src={images[index]} alt={project}/>
             </div>
             <div className="part2">
-            <a role="button" className="btn" href={`https://${project}`} target="_blank" rel="noopener noreferrer">Github</a>
-            <a role="button" className="btn2" href={`https://${project}`} target="_blank" rel="noopener noreferrer">Live</a>
+            <a role="button" className="btn" href={`https://github.com/Ckbhatia/${projectsUrl[index]}`} target="_blank" rel="noopener noreferrer">Github</a>
+            <a role="button" className="btn2" href={`https://${projectsUrl}`} target="_blank" rel="noopener noreferrer">Live</a>
             </div>
             </div>
         ))
