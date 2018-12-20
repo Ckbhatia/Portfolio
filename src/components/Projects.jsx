c239888f556d68736b67bab80ac811b2d4f8a421
Ckbhatia <chetansain86@gmail.com>
2018-12-20 13:28:09 +0530

    feat: added functionality to hide and show Live button

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,12 +9,16 @@ function importAll(r) {
 const images = importAll(require.context('../img/projects', false, /\.(png|jpe?g|svg)$/));
 
 //Experiments
-
+const buttonStyle = {
+    color: 'transparent'
+};
 
 export const FeaturedProjects =()=> { 
         
     const featuredProjectsName = ['React Neighborhood Map', 'Restaurant Reviews App', 'My Reads App', 'Feed Reader Testing'];
     const featuredProjectsUrl = ['Neighborhood-Map-React', 'mws-restaurant-stage-1', 'reactnd-myreads', 'frontend-nanodegree-feedreader'];
+    const liveUrl = ['','','',''];
+    
 
     return (
         featuredProjectsName.map((name, index) => 
@@ -25,7 +29,7 @@ export const FeaturedProjects =()=> {
                     </div>
                     <div className="part2">
                     <a role="button" className="btn" href={`https://github.com/Ckbhatia/${featuredProjectsUrl[index]}`} target="_blank" rel="noopener noreferrer">Github</a>
-                    <a role="button" className="btn2" href="https://" target="_blank" rel="noopener noreferrer">Live</a>
+                    <a role="button" className="btn2" href="https://" target="_blank" rel="noopener noreferrer">{liveUrl[index].length <= 1  ? null : 'Live'}</a>
                     </div>
                     </div>
             )               
@@ -33,8 +37,11 @@ export const FeaturedProjects =()=> {
 
 
 export const Projects =()=> {
+    
+    // Projects name, url and live url arrays
     const projectsName = ['Classic Arcade Game', 'Memory Game', 'Random Color picker', 'Portfolio', 'Pixel Art maker', 'Animal Trading Cards'];
     const projectsUrl = ['frontend-nanodegree-arcade-game', 'fend-project-memory-game', 'React-random-color-picker', 'Ckbhatia.github.io', 'project/editor/XWMgkQ', 'pen/zWbVwE'];
+    const liveUrl = ['','','','portfolio','project/full/169293', 'full/zWbVwE'];
 
     return (
             projectsName.map((project, index) => (
@@ -46,7 +53,7 @@ export const Projects =()=> {
             <div className="part2">
             {/* Button for gihub and live view the projects */}
             <a role="button" className="btn" href={index <= 3 ? `https://github.com/Ckbhatia/${projectsUrl[index]}` :  `https://codepen.io/ckbhatia/${projectsUrl[index]}`} target="_blank" rel="noopener noreferrer">{index <= 3 ? `Github`: 'Codepen'}</a>
-            <a role="button" className="btn2" href={`https://${projectsUrl}`} target="_blank" rel="noopener noreferrer">Live</a>
+            <a role="button" className="btn2" href={index <= 3 ? `https://ckbhatia.github.io/${liveUrl[index]}` :  `https://codepen.io/ckbhatia/${liveUrl[index]}`} target="_blank" rel="noopener noreferrer">{liveUrl[index].length <= 1  ? null : 'Live'}</a>
             </div>
             </div>
         ))
