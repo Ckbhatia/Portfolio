9ead5cf237b3454fc0c3a1b782f0516fb2f46fcb
Ckbhatia <chetansain86@gmail.com>
2019-02-05 15:16:29 +0530

    fix: add React.fragment and remove extra extension of import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import Main from './components/Main.jsx';
+import Main from './components/Main';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <React.Fragment>
         <Main />
-      </div>
+      </React.Fragment>
     );
   }
 }
